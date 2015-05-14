0fb515fc1eba220801105f97ceba9434e2a3cbe1
paul <paul@ingenuity.com>
2015-05-14 17:46:56 -0400

    mobile updates for phone size. new build generate in dist folder

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,16 @@
 (function (document) {
   'use strict';
  $('#tabs').carouFredSel({
-          circular: false,
+          //circular: false,
+          circular: true,
+    infinite: true,
           items: 1,
           width: '100%',
           auto: false,
-          pagination: {
-          container: '#pager > ul',
-          anchorBuilder: function( nr ) {if($(this).data('link')){ return '<li><a href="#">' + $(this).data('link') + '</a></li>';}}},
-          items: {width       : 200,height      : "50%"},
+          pagination: {container: '#pager > ul',
+          anchorBuilder: function( nr ) {if($(this).data('link')){ return '<li><a href="#">' + $(this).data('link') + '</a></li>';}}
+          },
+          items: {width: 200,height: "100%"},
           transition:true,
           responsive: true,
           //next:{button:'li.arrow-next'}
@@ -31,78 +33,51 @@
 
 /** TO DO: REFACTOR **/
 var doSmallMenu = window.matchMedia("screen and (max-width: 767px)").matches;
+
+
 if(doSmallMenu){
-  console.log('doing small menu');
   var curr = $('.nav li.selected');
   var curr_idx = curr.index();
   var toHide = curr.siblings().not(":first");
   toHide.addClass('inactive');
-  $('.nav').append('<li class="next-arrow"><span>&raquo;</span></li>');
-  $('.nav').prepend('<li class="prev-arrow inactive"><span>&laquo;</span></li>')
+  curr.addClass('the-first');
+  curr.next().addClass('the-second');
+  $('.nav li').eq(curr_idx+2).addClass('the-third');
+   $('.nav li').eq(curr_idx+3).addClass('the-fourth');
+   $('.nav li').eq(curr_idx+4).addClass('the-fifth');
+   $('.nav li').eq(curr_idx+4).addClass('the-sixth');
 }
+
 $('.nav > li a').on('click',function(){
   if(doSmallMenu){
+    var pager = document.querySelector('dotpager-nav');
     var curr = $(this).parent();
     var curr_idx = curr.index();
 
-    if(curr_idx == 6){
-      $('.prev-arrow').removeClass('inactive');
-      var $first = $('.nav > li').eq(1).detach(); 
+    if(curr_idx == 5){
+      var $first = $('.nav > li').eq(0).detach(); 
       curr.after($first);
       $first.addClass('selected');
-      var toHide = curr.siblings().not($first).not(".next-arrow");
+      var toHide = curr.siblings().not($first);
     }else{
-       $('.prev-arrow').addClass('inactive');
-       var toHide = curr.siblings().not(curr.next()).not(".next-arrow");
+       var toHide = curr.siblings().not(curr.next());
     }
+    if(curr.hasClass('the-first')) curr_idx = 6;
     $('.inactive').removeClass('inactive');
-    toHide.addClass('inactive');    
+    toHide.addClass('inactive');
+    if(curr_idx == 6){
+      $first = $('.nav li.the-first').detach();
+      $('.nav li.the-second').before($first);
+    }  
+    pager.advanceDots(curr_idx);  
     setTimeout(function(){
       $('.selected').removeClass('selected');
       curr.addClass('selected');
     },50); 
   }
 });
-$('.nav > li.next-arrow span').on('click',function(){
-  if(doSmallMenu){
-    var curr = $('.selected').next();
-    var curr_idx = curr.index();
-    if(curr_idx == 6){
-      $('.prev-arrow').removeClass('inactive');
-      var $first = $('.nav > li').eq(1).detach(); 
-      curr.after($first);
-      $first.addClass('selected');
-      var toHide = curr.siblings().not($first).not(".next-arrow");
-    }else{
-       $('.prev-arrow').addClass('inactive');
-       var toHide = curr.siblings().not(curr.next()).not(".next-arrow");
-    }
-    $('.inactive').removeClass('inactive');
-    toHide.addClass('inactive');    
-    setTimeout(function(){
-      $('.selected').removeClass('selected');
-      curr.addClass('selected');
-    },50); 
-    console.log('span clicked');
-    $('#tabs').trigger('next');
-    /*var curr = $(this).parent();
-    var curr_idx = curr.index();
 
-    if(curr_idx == 6){
-      $('.prev-arrow').removeClass('inactive');
-      var toHide = curr.siblings().not(":first").not(".next-arrow");
-    }else{
-       $('.prev-arrow').addClass('inactive');
-       var toHide = curr.siblings().not(curr.next()).not(".next-arrow");
-    }
-    $('.inactive').removeClass('inactive');
-    toHide.addClass('inactive');    
-    setTimeout(function(){
-      $('.selected').removeClass('selected');
-      curr.addClass('selected');
-    },50); */
-  }
-});
+
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
